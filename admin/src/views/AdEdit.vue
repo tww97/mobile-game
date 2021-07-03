11ec2aae3ec6096ecc72f2bb3b7e3cff0f4657b6
wenwotang <wenwotang97@163.com>
2021-07-04 01:09:02 +0800

    fix some bugs

--- admin/src/views/AdEdit.vue
+++ admin/src/views/AdEdit.vue
@@ -20,7 +20,8 @@
                         <el-form-item label='图标' style='margin-top:0.5rem'>
                             <el-upload
                             class="avatar-uploader"
-                            :action="$http.defaults.baseURL + '/upload'"
+                            :action="uploadUrl"
+                            :headers='getAuthHeaders()'
                             :show-file-list="false"
                             :on-success="res => $set(item, 'image', res.url)">
                             <img v-if="item.image" :src="item.image" class="avatar">
