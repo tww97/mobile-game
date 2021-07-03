a8ff6b50c1b6bd57bd05a60baa2a64e864ca5644
wenwotang <wenwotang97@163.com>
2021-07-04 00:28:26 +0800

    替换url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -62,7 +62,8 @@ module.exports = app => {
     //
     app.post('/admin/api/upload',authMiddleWare() , upload.single('file'), async (req, res) => {
         const file = req.file
-        file.url = `http://localhost:3000/uploads/${file.filename}`
+        // file.url = `http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://www.mobile-game.top/uploads/${file.filename}`
         res.send(file)
     })
 
